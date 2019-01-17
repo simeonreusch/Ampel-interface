565afb0b1b3eb02fdbbbe6ef80b1d22c677c92f1
vbrinnel <vsbrin@gmail.com>
2019-01-18 00:51:43 +0100

    TransientFlags: class is now abstract. New members

--- src/ampel/base/flags/TransientFlags.py
+++ src/ampel/base/flags/TransientFlags.py
@@ -4,14 +4,16 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 04.07.2018
+# Last Modified Date: 18.01.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.base.flags.AmpelMetaFlags import AmpelMetaFlags
+from abc import ABCMeta
 
-class TransientFlags(metaclass=AmpelMetaFlags):
+class TransientFlags(metaclass=ABCMeta):
 	"""
-	First 20 powers of two are reserved for general ampel flags
 	"""
-	HAS_ERROR                   = 1048576
-	T1_AUTO_COMPLETE            = 2097152
+	HAS_ERROR                   = ()
+	HAS_TNS_NAME                = ()
+	MIXED_DATA_SOURCE           = ()
+	T1_AUTO_COMPLETE            = ()
