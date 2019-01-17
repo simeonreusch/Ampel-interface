870c67de6e0dbc833672dcef7455453294b78bb7
vbrinnel <vsbrin@gmail.com>
2019-01-18 00:50:48 +0100

    PhotoFlags: class is now abstract. Removed ZTF specific members

--- src/ampel/base/flags/PhotoFlags.py
+++ src/ampel/base/flags/PhotoFlags.py
@@ -4,33 +4,20 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 04.07.2018
+# Last Modified Date: 18.01.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.base.flags.AmpelMetaFlags import AmpelMetaFlags
+from abc import ABCMeta
 
-class PhotoFlags(metaclass=AmpelMetaFlags):
-
+class PhotoFlags(metaclass=ABCMeta):
 	"""
-	First 20 powers of two are reserved for general ampel flags
-	Flags related to photometric measurements, 
-	used in the photopoint and upper limit documents.
-	This class can contain more than 64 different flags.
 	"""
 
-	PHOTOPOINT                = 1048576
-	UPPERLIMIT                = 2097152
-	BAND_ZTF_G                = 4194304
-	BAND_ZTF_R                = 8388608
-	BAND_ZTF_I                = 16777216
-	ZTF_COLLAB                = 33554432
-	ZTF_PUBLIC                = 67108864
-	ZTF_HIGH_CADENCE          = 134217728
+	PHOTOPOINT                = ()
+	UPPERLIMIT                = ()
+	SUPERSEDED                = ()
 
-	SUPERSEDED                = 268435456
-	HAS_HUMBOLDT_ZP           = 536870912
-
-	IMAGE_BAD_CALIBRATION     = 1073741824
-	IMAGE_ARTIFACT            = 2147483648
-	IMAGE_TRACKING_PBLM       = 4294967296
-	IMAGE_FOCUS_PBLM          = 8589934592
+	IMAGE_BAD_CALIBRATION     = ()
+	IMAGE_ARTIFACT            = ()
+	IMAGE_TRACKING_PBLM       = ()
+	IMAGE_FOCUS_PBLM          = ()
