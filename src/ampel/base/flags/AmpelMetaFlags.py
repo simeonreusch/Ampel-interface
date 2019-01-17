0650b5183ba9803c938f50dab099a627293ef271
vbrinnel <vsbrin@gmail.com>
2019-01-18 00:49:44 +0100

    AmpelMetaFlags: enum inheritance now customizable

--- src/ampel/base/flags/AmpelMetaFlags.py
+++ src/ampel/base/flags/AmpelMetaFlags.py
@@ -4,17 +4,26 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.06.2018
-# Last Modified Date: 04.07.2018
+# Last Modified Date: 18.01.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from enum import IntFlag
-from ampel.base.flags.AmpelFlags import AmpelFlags
 
 class AmpelMetaFlags(type):             
 
-	def __new__(metacls, name, bases, d):
-		flags = [(i.name, i.value) for i in AmpelFlags]
-		for k,v in d.items():
-			if not k.endswith('__'):
-				flags.append((k,v))
-		return IntFlag(name, flags, module=d['__module__'])
+    def __new__(metacls, name, bases, d, extends):
+
+        flags=[]
+        i=1
+
+        for el in extends if type(extends) is list else [extends]:
+            for ell in el:
+                flags.append((ell.name, 2**i))
+                i+=1
+
+        for k in d.keys():
+            if not k.endswith('__'):
+                flags.append((k, 2**i))
+                i+=1
+
+        return IntFlag(name, flags, module=d['__module__'])
