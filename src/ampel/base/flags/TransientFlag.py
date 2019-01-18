0f040f63b9068683feb0c2b68464357bf116dcd9
vbrinnel <vsbrin@gmail.com>
2019-01-18 11:10:31 +0100

    TransientFlags -> TransientFlag rename

--- src/ampel/base/flags/TransientFlag.py
+++ src/ampel/base/flags/TransientFlag.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/base/flags/TransientFlags.py
+# File              : ampel/base/flags/TransientFlag.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
 # Last Modified Date: 18.01.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.base.flags.AmpelMetaFlags import AmpelMetaFlags
 from abc import ABCMeta
 
-class TransientFlags(metaclass=ABCMeta):
+class TransientFlag(metaclass=ABCMeta):
 	"""
 	"""
 	HAS_ERROR                   = ()
