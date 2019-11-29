0db108d4a20df3152f1d08d3dd782aad404f1fd9
vbrinnel <valery.brinnel@gmail.com>
2019-11-29 11:50:34 +0100

    AbsT0AlertFilter: added InitConfig definition

--- src/ampel/abstract/AbsT0AlertFilter.py
+++ src/ampel/abstract/AbsT0AlertFilter.py
@@ -4,18 +4,29 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 22.10.2019
+# Last Modified Date: 31.10.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
+from typing import Set, Optional, Sequence
+from pydantic import validator
+from ampel.model.UnitData import UnitData
+from ampel.object.AmpelAlert import AmpelAlert
 from ampel.abstract.AmpelABC import abstractmethod
 from ampel.abstract.AbsAmpelUnit import AbsAmpelUnit
-from ampel.base.AmpelAlert import AmpelAlert
-from typing import Set, Optional
+from ampel.model.AmpelBaseModel import AmpelBaseModel
+
 
 class AbsT0AlertFilter(AbsAmpelUnit, abstract=True):
 	"""
 	Base class for T0 filters
 	"""
+
+	class InitConfig(AmpelBaseModel):
+		""" 
+		"""
+		t2_compute: Optional[Sequence[UnitData]]
+		on_match_t2_units: Optional[Sequence[str]] = ()
+
 
 	@abstractmethod
 	def apply(self, ampel_alert: AmpelAlert) -> Optional[Set[str]]:
