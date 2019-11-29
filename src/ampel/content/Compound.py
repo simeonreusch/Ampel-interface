8bb892fc9ccb077826641995c64dc613f3bba91e
vbrinnel <valery.brinnel@gmail.com>
2019-11-29 11:38:04 +0100

    Compound: v0.7 compatibility update

--- src/ampel/content/Compound.py
+++ src/ampel/content/Compound.py
@@ -1,17 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/content/Compound.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : Unspecified
+# Last Modified Date: 18.12.2019
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from dataclasses import dataclass
-from typing import Any
+from dataclasses import dataclass, _MISSING_TYPE
+from typing import Any, Sequence, Union, Optional, Dict
+
+none_type = type(None)
 
 @dataclass(frozen=True)
-class Compound:
-	id: Any
-	comp: Any = None
-	added: Any = None
-	alDocType: Any = None
-	alTags: Any = None
-	lastJD: Any = None
-	len: int = None
-	ppId: Any = None
-	tier: int = None
-	tranId: Any = None
-	flavors: Any = None
+class Compound: 
+
+	id: Union[int, str, Dict]
+	tags: Optional[Sequence[Union[int, str]]]
+	stock: Union[int, str, Sequence[Union[int, str]]]
+	channels: Sequence[Union[int, str]]
+	data: Sequence[Dict[str, Any]]
+	added: float
+	tier: int
+	len: int
+
+	# Ignore extras, enable aliases, set defaults (none for now)
+	def __init__(self, **kwargs):
+
+		# pylint: disable=no-member
+		fields = self.__dataclass_fields__
+		d = self.__dict__
+
+		# Aliases
+		if '_id' in kwargs:
+			d['id'] = kwargs['_id']
+
+		for k in fields:
+			if k in kwargs:
+				d[k] = kwargs[k]
+			else:
+				if k in d: # field value already set by aliases
+					continue
+				if '__args__' in fields[k].type.__dict__ and none_type in fields[k].type.__args__:
+					d[k] = None
+					continue # Optional type
+				# No default value
+				if fields[k].default.__class__ is _MISSING_TYPE:
+					raise ValueError(f"Value missing for field '{k}'")
+				d[k] = fields[k].default
